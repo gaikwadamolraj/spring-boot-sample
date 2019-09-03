df8de44fceee0b6ea5d26eb91495b97a95f22b7c
Amol Gaikwad <amol.gaikwad11@wipro.com>
2019-09-03 15:41:52 +0100

    productId changes and lombok added

--- inventory-service/src/main/java/com/wipro/inventory/SKU.java
+++ inventory-service/src/main/java/com/wipro/inventory/SKU.java
@@ -1,49 +1,13 @@
 package com.wipro.inventory;
 
+import lombok.Data;
+
+@Data
 public class SKU {
-	private Integer productID;
+	private Integer productId;
 	private String name;
 	private Integer id;
 	private String description;
 	private Double price;
 	private Integer count;
-	public Integer getProductID() {
-		return productID;
-	}
-	public void setProductID(Integer productID) {
-		this.productID = productID;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-	public Integer getCount() {
-		return count;
-	}
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-	
-
-
 }
